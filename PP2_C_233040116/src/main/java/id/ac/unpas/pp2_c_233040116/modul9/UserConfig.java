e9f763eca414df56a0b382dccc8a950c559a4ba8
haekal3 <haekalkhadafi21@gmail.com>
2025-12-15 21:52:07 +0700

    modul 9

---
+++ PP2_C_233040116/src/main/java/id/ac/unpas/pp2_c_233040116/modul9/UserConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.pp2_c_233040116.modul9;
+
+/**
+ *
+ * @author KATANA 15
+ */
+
+import java.io.Serializable;
+
+public class UserConfig implements Serializable {
+    private String username;
+    private int fontsize;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getFontsize() {
+        return fontsize;
+    }
+
+    public void setFontsize(int fontsize) {
+        this.fontsize = fontsize;
+    }
+}
